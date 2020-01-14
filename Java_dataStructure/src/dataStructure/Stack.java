f4504a1cbb8b65972ee5c67f3e526973fcafcc59
likem <likem@114.200.165.56>
2020-01-14 23:52:19 +0900

    스택

--- Java_dataStructure/src/dataStructure/Stack.java
+++ Java_dataStructure/src/dataStructure/Stack.java
@@ -2,6 +2,19 @@ package dataStructure;
 
 public class Stack {
 	
-	
+	public static void main(String[] args) {
+		
+		java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
+		
+		System.out.println("Stack created : ");
+		
+		for(int i=0; i<10; i++) 
+		stack.push(new Integer(i)); //0~9의 수로 스택을 구성한다.
+		
+		System.out.println("1pop : " + stack.pop()); //스택의 값을 뺀다
+		System.out.println("2pop : " + stack.pop()); //스택의 값을 뺀다
+		System.out.println("3pop : " + stack.pop()); //스택의 값을 뺀다
+		System.out.println("4pop : " + stack.pop()); //스택의 값을 뺀다
+	}
 
 }
